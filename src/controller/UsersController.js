82dbf28b8dd483f128fe47f1527dd6fc34014d2c
erozonachi <erozonachi.james@gmail.com>
2019-07-10 16:02:14 +0100

    Add insert-user method to the users-controller class

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,3 +1,16 @@
+import UserModel from '../../users/userDb';
+
 export class UsersController {
-  
+  static async insertUser(req, res) {
+    try {
+      const { name } = req.body;
+      const user = await UserModel.insert({ name });
+
+      res.status(201).json(user[0]);
+    } catch(error) {
+      res.status(500).json({
+        error: 'user could not be save at this time'
+      });
+    }
+  }
 }
