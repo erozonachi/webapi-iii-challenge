f3c8d01560f98317803d0285c0c5f79da81aa2af
erozonachi <erozonachi.james@gmail.com>
2019-07-10 14:59:06 +0100

    Import and use logger function in server entry file

--- src/helper/logger.js
+++ src/helper/logger.js
@@ -1,3 +1,4 @@
-export function logger(req, res) {
+export function logger(req, res, next) {
   console.log(`URL: ${req.url} | Method: ${req.method} | Timestamp: ${Date.now()}`);
+  next();
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { logger } from './helper';
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Web API III Challenge by James Eneh'});
