aeec6be25c2a28a5193032ffa9c6ad30b8b7168a
erozonachi <erozonachi.james@gmail.com>
2019-07-10 15:01:06 +0100

    Import and use helmet in server entry file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import helmet from 'helmet';
 import { logger } from './helper';
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(express.json());
+server.use(helmet());
 server.use(logger);
 
 server.get('/', (req, res) => {
