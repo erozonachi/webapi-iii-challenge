65a67d67038dda2c79a1bfa571e2d190d5c68f74
erozonachi <erozonachi.james@gmail.com>
2019-07-10 15:44:42 +0100

    Add validate-post method to the validator class

--- src/helper/Validator.js
+++ src/helper/Validator.js
@@ -6,10 +6,9 @@ export class Validator {
       const { id } = req.params;
       
       if(!Number.isInteger(Number.parseInt(id, 10))) {
-        res.status(400).json({
+        return res.status(400).json({
           message: 'Malformed ID'
         });
-        return;
       }
       const user = await userDb.getById(id);
       if(user.length === 1) {
@@ -40,4 +39,19 @@ export class Validator {
     }
     next();
   }
+
+  static validatePost(req, res, next) {
+    if(!req.body) {
+      return res.status(400).json({
+        message: 'missing post data'
+      });
+    }
+    if(!req.body.name || req.body.name.trim() === '') {
+      return res.status(400).json({
+        message: 'missing required text field'
+      });
+    }
+    next();
+  }
+
 }
