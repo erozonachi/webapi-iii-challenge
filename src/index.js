ff34678ffc689ccb5ad0e2d2db0a49e1550dfab6
erozonachi <erozonachi.james@gmail.com>
2019-07-10 14:24:42 +0100

    Setup a basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// code away!
+import express from 'express';
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Web API III Challenge by James Eneh'});
+});
+
+server.listen(port, error => {
+  if(error) {
+    console.log(error);
+    return;
+  }
+  console.log(`Server running on port: ${port}`);
+});
