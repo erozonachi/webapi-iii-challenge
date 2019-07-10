350ebeef1f99584d0613f521a4c72e7aa88d5294
erozonachi <erozonachi.james@gmail.com>
2019-07-10 20:03:23 +0100

    Add update-post method to posts controller

--- src/controller/PostsController.js
+++ src/controller/PostsController.js
@@ -1,5 +1,17 @@
 import PostModel from '../../posts/postDb';
 
 export class PostsController {
-  
+  static async updatePost(req, res) {
+    try {
+      const { text } = req.body;
+      await PostModel.update(req.post.id, { text });
+      const post = await PostModel.getById(req.post.id);
+
+      res.status(200).json(post);
+    } catch(error) {
+      res.status(500).json({
+        error: 'post could not be modify at this time'
+      });
+    }
+  }
 }
