0bcb77b962d3d3da93dd9f1ace8dc0160cba8bec
erozonachi <erozonachi.james@gmail.com>
2019-07-10 15:27:45 +0100

    Add validate-user-id method to the validator class

--- src/helper/Validator.js
+++ src/helper/Validator.js
@@ -1,5 +1,29 @@
 import userDb from '../../users/userDb';
 
 export class Validator {
-  
+  static async validateUserId(req, res, next) {
+    try {
+      const { id } = req.params;
+      
+      if(!Number.isInteger(Number.parseInt(id, 10))) {
+        res.status(400).json({
+          message: 'Malformed ID'
+        });
+        return;
+      }
+      const user = await userDb.getById(id);
+      if(user.length === 1) {
+        req.user = user[0];
+        next();
+      } else {
+        res.status(400).json({
+          message: 'invalid user id'
+        });
+      }
+    } catch(error) {
+      res.status(500).json({
+        error: 'Request could not be completed at this time, try again'
+      });
+    }
+  }
 }
